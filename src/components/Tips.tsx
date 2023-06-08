fa4f5874dbbafc9a98739fda1611a98d8f45d435
Frederico Floryan <fredericofloryan@Fredericos-Macbook-Pro.local>
2023-06-08 11:00:16 +0800

    improve tips UI

--- src/components/Tips.tsx
+++ src/components/Tips.tsx
@@ -9,27 +9,9 @@ function Tips() {
   return (
     <div className="flex flex-col items-start justify-center h-full">
       <div className="flex flex-col items-start justify-center space-y-2">
-        <div className="flex flex-row items-center space-x-2 pb-2">
+        <div className="lex flex-row items-center space-x-2 pb-2">
           <BlubIcon className="w-8 h-8 text-gray-800" />
           <div className="text-xl font-medium text-gray-800">{i18n.t('common.tips') as string}</div>
-        </div>
-        <div className="text-gray-800 leading-7 flex flex-row">
-          <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
-          <div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: i18n.t('tips.tips1') as string,
-              }}
-            />
-          </div>
-        </div>
-        <div className="text-gray-800 leading-7 flex flex-row">
-          <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
-          <div
-            dangerouslySetInnerHTML={{
-              __html: i18n.t('tips.tips2') as string,
-            }}
-          />
         </div>
         <div className="text-gray-800 leading-7 flex flex-row">
           <RightTriangleIcon className="w-7 h-7 text-gray-800 shrink-0" strokeWidth="1.5" />
