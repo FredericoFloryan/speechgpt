74c385852d2d3ad85155471bc812930ca21fff03
Frederico Floryan <fredericofloryan@Fredericos-Macbook-Pro.local>
2023-06-08 14:27:03 +0800

    disable Amazon polly and System option in settings

--- src/components/Settings/SynthesisSection.tsx
+++ src/components/Settings/SynthesisSection.tsx
@@ -27,21 +27,15 @@ interface SynthesisSectionProps {}
 
 const SynthesisSection: React.FC<SynthesisSectionProps> = ({}) => {
   const { key, setKey, speech, setSpeech } = useGlobalStore();
+  useEffect(() => {
+    if(speech.service !== 'Azure TTS') {
+      setSpeech({ ...speech, service: 'Azure TTS' });
+    }
+  }, []);
+
   const { i18n } = useTranslation();
 
-  const speechSynthesisServices = isMobile
-    ? ['Azure TTS', 'Amazon Polly']
-    : ['System', 'Azure TTS', 'Amazon Polly'];
-
-  function getSystemLanguageCode(language: string) {
-    return Object.keys(speechSynthesisSystemLanguages).find(
-      key => speechSynthesisSystemLanguages[key] === language
-    );
-  }
-
-  function getAmazonPollyLanguageCode(language: string) {
-    return Object.keys(amazonPollyLanguages).find(key => amazonPollyLanguages[key] === language);
-  }
+  const speechSynthesisServices = ['Azure TTS']
 
   function getAzureTTSLanguageCode(language: string) {
     return Object.keys(azureSpeechSynthesisLanguagesLocale).find(
@@ -78,12 +72,6 @@ const SynthesisSection: React.FC<SynthesisSectionProps> = ({}) => {
     }
   }, [key.azureRegion]);
 
-  useEffect(() => {
-    if (!key.awsRegion) {
-      setKey({ ...key, awsRegion: awsRegions[0] });
-    }
-  }, [key.awsRegion]);
-
   const filteredVoices = systemVoices.filter(
     voice => voice.lang.split('-')[0] === speech.systemLanguage
   );
@@ -94,55 +82,6 @@ const SynthesisSection: React.FC<SynthesisSectionProps> = ({}) => {
       <SettingGroup>
         {isMobile && (
           <SettingWarningText text={i18n.t('setting.synthesis.mobile-not-supported') as string} />
-        )}
-        <SettingSelect
-          text={i18n.t('setting.synthesis.synthesis-service') as string}
-          helpText={i18n.t('setting.synthesis.synthesis-service-tooltip') as string}
-          options={speechSynthesisServices}
-          value={speech.service}
-          selectClassName={'flex flex-col sm:flex-row justify-between space-y-2 sm:space-y-0'}
-          onChange={e => setSpeech({ ...speech, service: e })}
-        />
-        {speech.service === 'Amazon Polly' && (
-          <>
-            {existEnvironmentVariable('AWS_REGION') &&
-            existEnvironmentVariable('AWS_ACCESS_KEY_ID') &&
-            existEnvironmentVariable('AWS_ACCESS_KEY') ? (
-              <SettingCheckText
-                text={i18n.t('setting.synthesis.polly-already-set-environment-variable') as string}
-              />
-            ) : (
-              <>
-                <SettingSelect
-                  text={i18n.t('setting.synthesis.aws-region') as string}
-                  options={awsRegions}
-                  value={key.awsRegion}
-                  selectClassName={
-                    'flex flex-col sm:flex-row justify-between space-y-2 sm:space-y-0'
-                  }
-                  onChange={e => setKey({ ...key, awsRegion: e })}
-                />
-                <SettingInput
-                  text={i18n.t('setting.synthesis.aws-access-key-id') as string}
-                  id={'aws-access-key-id'}
-                  type={'text'}
-                  value={key.awsKeyId}
-                  placeholder={i18n.t('setting.synthesis.aws-access-key-id-placeholder') as string}
-                  onChange={e => setKey({ ...key, awsKeyId: e })}
-                />
-                <SettingInput
-                  text={i18n.t('setting.synthesis.aws-secret-access-key') as string}
-                  id={'aws-secret-access-key'}
-                  type={'text'}
-                  value={key.awsKey}
-                  placeholder={
-                    i18n.t('setting.synthesis.aws-secret-access-key-placeholder') as string
-                  }
-                  onChange={e => setKey({ ...key, awsKey: e })}
-                />
-              </>
-            )}
-          </>
         )}
         {speech.service === 'Azure TTS' && (
           <>
@@ -174,95 +113,6 @@ const SynthesisSection: React.FC<SynthesisSectionProps> = ({}) => {
           </>
         )}
       </SettingGroup>
-
-      {speech.service === 'System' && (
-        <>
-          <SettingDivider />
-          <SettingTitle text={i18n.t('setting.synthesis.properties') as string} />
-          <SettingGroup>
-            <SettingSelect
-              text={i18n.t('setting.synthesis.language') as string}
-              options={systemLanguages.map(language => speechSynthesisSystemLanguages[language])}
-              value={speechSynthesisSystemLanguages[speech.systemLanguage]}
-              selectClassName={'flex flex-col sm:flex-row justify-between space-y-2 sm:space-y-0'}
-              onChange={e =>
-                setSpeech({
-                  ...speech,
-                  systemLanguage: getSystemLanguageCode(e),
-                })
-              }
-            />
-            <SettingSlider
-              text={i18n.t('setting.synthesis.speech-rate') as string}
-              id={'speech-rate'}
-              value={speech.systemRate}
-              onChange={e => setSpeech({ ...speech, systemRate: e })}
-              min={'0.5'}
-              max={'2'}
-              step={'0.05'}
-            />
-            <SettingSlider
-              text={i18n.t('setting.synthesis.pitch') as string}
-              id={'pitch'}
-              value={speech.systemPitch}
-              onChange={e => setSpeech({ ...speech, systemPitch: e })}
-              min={'0.5'}
-              max={'2'}
-              step={'0.05'}
-            />
-          </SettingGroup>
-
-          <SettingDivider />
-          <SettingTitle text={i18n.t('setting.synthesis.voice') as string} />
-          <SettingGroup>
-            <SettingSelect
-              text={i18n.t('setting.synthesis.voice-id') as string}
-              helpText={i18n.t('setting.synthesis.voice-id-tooltip') as string}
-              options={filteredVoices.map(voice => voice.name)}
-              value={speech.systemVoice}
-              className="w-56 pr-8"
-              selectClassName={'flex flex-col sm:flex-row justify-between space-y-2 sm:space-y-0'}
-              onChange={e => setSpeech({ ...speech, systemVoice: e })}
-            />
-          </SettingGroup>
-        </>
-      )}
-
-      {speech.service === 'Amazon Polly' && (
-        <>
-          <SettingDivider />
-          <SettingTitle text={i18n.t('setting.synthesis.properties') as string} />
-          <SettingGroup>
-            <SettingSelect
-              text={i18n.t('setting.synthesis.language') as string}
-              className={'min-w-min pr-8'}
-              options={Object.values(amazonPollyLanguages)}
-              value={amazonPollyLanguages[speech.pollyLanguage]}
-              onChange={e =>
-                setSpeech({
-                  ...speech,
-                  pollyLanguage: getAmazonPollyLanguageCode(e),
-                })
-              }
-            />
-          </SettingGroup>
-
-          <SettingDivider />
-          <SettingTitle text={i18n.t('setting.synthesis.voice') as string} />
-          <SettingGroup>
-            <SettingSelect
-              text={i18n.t('setting.synthesis.engine') as string}
-              helpText={i18n.t('setting.synthesis.engine-tooltip') as string}
-              options={Object.values(amazonPollyEngines)}
-              value={speech.pollyEngine}
-              className="min-w-min pr-8"
-              onChange={e => setSpeech({ ...speech, pollyEngine: e })}
-            />
-            <PollyVoice />
-          </SettingGroup>
-        </>
-      )}
-
       {speech.service === 'Azure TTS' && (
         <>
           <SettingDivider />
@@ -282,7 +132,6 @@ const SynthesisSection: React.FC<SynthesisSectionProps> = ({}) => {
               }
             />
           </SettingGroup>
-
           <SettingDivider />
           <SettingTitle text={i18n.t('setting.synthesis.voice') as string} />
           <SettingGroup>
